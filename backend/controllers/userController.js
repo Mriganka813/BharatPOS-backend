089528085d8fa46e6b88920597c40156fec12836
Sanket2055 <sanketdiwate95@gmail.com>
2022-06-14 22:42:54 +0530

    minor change

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -94,6 +94,10 @@ exports.signUpWithPhoneNumber = catchAsyncErrors(async (req, res, next) => {
   });
 
   const { email, password, businessName, businessType, address } = req.body;
+  const data = await User.findOne({phoneNumber:phoneNumber});
+  if(data){
+    return next(new ErrorHandler("Phone Number already registered , Sign In instead", 400));
+  }
   const user = await User.create({
     email,
     password,
