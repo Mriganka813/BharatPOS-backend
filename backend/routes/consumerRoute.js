dc27bfaca52061b0676cd931220cc53acba7ef58
Sanket2055 <sanketdiwate95@gmail.com>
2022-07-22 17:59:28 +0530

    fixed code pattern

--- backend/routes/consumerRoute.js
+++ backend/routes/consumerRoute.js
@@ -45,46 +45,29 @@ router.route("/sellers/all").get(isAuthenticatedConsumer, getAllUserDetails);
 router.route("/seller/:id").get(isAuthenticatedConsumer, getSingleUserDetail);
 
 router.route("/product/:id").get(isAuthenticatedConsumer, getInventoryDetails);
-router
-  .route("/products/all")
-  .get(isAuthenticatedConsumer, getAllInventorieswithSearch);
 
-router
-  .route("/sellercontact/:id")
-  .get(isAuthenticatedConsumer, getContactNumber);
+router.route("/products/all").get(isAuthenticatedConsumer, getAllInventorieswithSearch);
 
-router
-  .route("/getSellersAndSearch")
-  .get(isAuthenticatedConsumer, getSellersAndSearch);
+router.route("/sellercontact/:id").get(isAuthenticatedConsumer, getContactNumber);
+
+router.route("/getSellersAndSearch").get(isAuthenticatedConsumer, getSellersAndSearch);
 
 router.route("/sellers").get(isAuthenticatedConsumer, getSellers);
 
-router
-  .route("/sellerProduct/:id")
-  .get(isAuthenticatedConsumer, getProductsOfUser);
+router.route("/sellerProduct/:id").get(isAuthenticatedConsumer, getProductsOfUser);
 
 router.route("/sellers/search").get(isAuthenticatedConsumer, getSellersByName);
 
-router
-  .route("/productname/search")
-  .get(isAuthenticatedConsumer, getProductNamesandSearch);
+router.route("/productname/search").get(isAuthenticatedConsumer, getProductNamesandSearch);
 
-router
-  .route("/product/click/:id")
-  .get(isAuthenticatedConsumer, addClickProduct);
+router.route("/product/click/:id").get(isAuthenticatedConsumer, addClickProduct);
 
 router.route("/seller/click/:id").get(isAuthenticatedConsumer, addClickSeller);
 
-router
-  .route("/products/popular")
-  .get(isAuthenticatedConsumer, getTopClickedProducts);
+router.route("/products/popular").get(isAuthenticatedConsumer, getTopClickedProducts);
 
-router
-  .route("/popular/seller")
-  .get(isAuthenticatedConsumer, getTopClickedSellers);
+router.route("/popular/seller").get(isAuthenticatedConsumer, getTopClickedSellers);
 
-router
-  .route("/popular/seller")
-  .get(isAuthenticatedConsumer, getTopClickedSellers);
+router.route("/popular/seller").get(isAuthenticatedConsumer, getTopClickedSellers);
 
 module.exports = router;
