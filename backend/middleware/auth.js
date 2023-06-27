a082eb9f26db415471e517bf557e64d3a52a0532
raj <rpbarmaiya@gmail.com>
2023-06-27 22:32:51 +0530

    fix duplicate gstID and login

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -10,11 +10,9 @@ exports.isAuthenticatedUser = catchAsyncErrors(async (req, res, next) => {
   // console.log(req.cookies);
   
   // const { token } = req.cookies;
-  // const { token } = req.cookies;
+  const { token } = req.cookies;
 
-  const { authorization } = req.headers;
-  // console.log(authorization);
-  const token = authorization
+
   console.log(token);
   if (!token) {
     return next(new ErrorHandler("Please login to access this resource", 401));
