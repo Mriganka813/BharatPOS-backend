3e17437ae4bd3cb1919153ba0bb419bced3d0bc3
Himanshi Sharma <himanshish456@gmail.com>
2023-08-03 18:52:23 +0530

    auth removed

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -52,7 +52,7 @@ router.route("/signup/otp").post(signUpWithPhoneNumber);
 
 router.route("/password/reset").put(resetPassword);
 
-router.route("/getupi/:userId").get(isAuthenticatedUser, getUpi);
+router.route("/getupi/:userId").get(getUpi);
 
 router.route("/upi/updateupi").put(isAuthenticatedUser, updateUpi);
 
