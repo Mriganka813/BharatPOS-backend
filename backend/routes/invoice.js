27c88d28a7873350832c06932b19ee22e7d9ea84
raj <rpbarmaiya@gmail.com>
2023-09-13 16:29:55 +0530

    render html page

--- backend/routes/invoice.js
+++ backend/routes/invoice.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const ejs = require('ejs'); 
 
-
-router.post('/invoice',async(req,res)=>{
+router.post('/invoice5',async(req,res)=>{
   
   const {orderItem,invoice,address,companyName,email,phone,date} = req.body
 
@@ -22,10 +22,34 @@ router.post('/invoice',async(req,res)=>{
     console.log(err);
    }
 })
-// ammt = (rate+gst)*qty
-// subTotal = total of qty * rate
-// gsttotal = qty * gst
-// bet total = gst+subtotal
+
+
+router.post('/invoice', async (req, res) => {
+  const { orderItem, invoice, address, companyName, email, phone, date } = req.body;
+
+  try {
+    // Render the 'invoice.ejs' file with the provided data
+    const htmlContent = await ejs.renderFile('views/invoice.ejs', {
+      companyName,
+      email,
+      phone,
+      address,
+      invoice,
+      orderItem,
+      date
+    });
+
+    // Set the Content-Type header to specify that we are sending HTML content
+    res.setHeader('Content-Type', 'text/html');
+
+    // Send the HTML content as the response
+    res.send(htmlContent);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+module.exports = router;
 
 
 
