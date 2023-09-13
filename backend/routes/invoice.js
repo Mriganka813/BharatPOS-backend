1ff34525611c5284637dbb811fc2fa2ce05cba97
raj <rpbarmaiya@gmail.com>
2023-09-13 17:25:46 +0530

    invoice

--- backend/routes/invoice.js
+++ backend/routes/invoice.js
@@ -1,35 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const ejs = require('ejs'); 
+const Invoice = require('../models/invoice')
 
-router.post('/invoice5',async(req,res)=>{
+router.post('/invoice',async(req,res)=>{
   
   const {orderItem,invoice,address,companyName,email,phone,date} = req.body
 
+  
+
    try{
-    return res.render('invoice',{
-        companyName,
-        email,
-        phone,
-        address,
-        invoice,
-        orderItem,
-        date
 
-
-    })
-   }catch(err){
-    console.log(err);
-   }
-})
-
-
-router.post('/invoice', async (req, res) => {
-  const { orderItem, invoice, address, companyName, email, phone, date } = req.body;
-
-  try {
-    // Render the 'invoice.ejs' file with the provided data
-    const htmlContent = await ejs.renderFile('views/invoice.ejs', {
+    const newInvoice= new Invoice({
       companyName,
       email,
       phone,
@@ -37,13 +19,45 @@ router.post('/invoice', async (req, res) => {
       invoice,
       orderItem,
       date
-    });
+})
 
-    // Set the Content-Type header to specify that we are sending HTML content
-    res.setHeader('Content-Type', 'text/html');
+await newInvoice.save();
+console.log(newInvoice);
+    return res.send(newInvoice._id)
+   }catch(err){
+    console.log(err);
+   }
+})
 
-    // Send the HTML content as the response
-    res.send(htmlContent);
+//  6501a0fcf99014efc41ca05f
+
+router.get('/genrate/:id', async (req, res) => {
+  const {id} = req.params;
+
+  try {
+    const invoiceid = await Invoice.findById(id)
+
+    const  orderItem = invoiceid.orderItem
+    const   companyName = invoiceid.companyName 
+    const   email = invoiceid.email
+    const   phone = invoiceid.phone
+    const   address = invoiceid.address
+    const   invoice = invoiceid.invoice
+    const   date = invoiceid.date
+
+
+    return res.render('invoice',{
+      companyName,
+      email,
+      phone,
+      address,
+      invoice,
+      orderItem,
+      date
+
+
+  })
+
   } catch (err) {
     console.log(err);
   }
