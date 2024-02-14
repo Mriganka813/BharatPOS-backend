432e7b442ea20da1bb303b3bdc2f255d63c0a6bb
Mriganka813 <mriganka.81349@gmail.com>
2024-02-14 15:18:34 +0530

    KOT & billingorders

--- backend/models/kotModel.js
+++ backend/models/kotModel.js
@@ -1,43 +1,34 @@
 const mongoose = require("mongoose");
 
+const itemDetails = mongoose.Schema({
+   name: {
+      type: String,
+      required: true
+   },
+   quantity: {
+      type: Number,
+      // required: true
+   },
+   createdAt: {
+      type: String,
+      required: true
+   }
+})
+
 const kotSchema = mongoose.Schema({
- item:{
-    type:String
- },
- date:{
-    type:String
- },
- qty:{
-    type:Number
- },
- user:{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "user",
- }
+   kotId: {
+      type: String
+   },
+   item: {
+      type: [itemDetails]
+   },
+   date: {
+      type: String
+   },
+   user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+   }
 });
 
 module.exports = mongoose.model("kotModel", kotSchema);
-
-
-// sample data
-// {
-//     "invoiceNum":"B257",
-//     "guestName":"Raj",
-//     "nuberOfGuest":2,
-//     "address":{
-//         "city":"Mandla",
-//         "state":"MP",
-//         "country": "INDIA",
-//         "streetAddress":"Mandla"
-//     },
-//     "number":700061754,
-//     "company":"MagicStep Solution",
-//     "gst":"BI7987985",
-//     "checkIn":"08/08/2023",
-//     "checkOut":"10/08/2023",
-//     "days":2,
-//     "roomNo":"102A",
-//     "otherCharges":50,
-//     "adv":500,
-//     "dis":200
-// }
